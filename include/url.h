f96cfb1f9a252b2ebd9f8025ea31e1b18db6a077
Anthonny Gabriell Marins Alves <anthonny14gabriell@gmail.com>
2026-05-07 17:20:32 -0400

    feat(url): adaptando o arquivo de cabeçalho da entidade url

--- include/url.h
+++ include/url.h
@@ -2,14 +2,15 @@
 #define GERENCIADOR_DE_URLS_URL_H
 
 struct url {
-    char endereco[2083];
-    char data[11];
-    char hora[9];
+    char* endereco;
+    char* data;
+    char* hora;
 };
 
 typedef struct url t_url;
 
 t_url* ler_url(char* linha);
 void editar_url(t_url* url);
+void liberar_url(t_url* url);
 
 #endif //GERENCIADOR_DE_URLS_URL_H
