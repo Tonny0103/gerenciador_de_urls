03acbc3240eb93a956a0b1bacd23a0517bdd0b29
Anthonny Gabriell Marins Alves <anthonny14gabriell@gmail.com>
2026-05-08 14:22:30 -0400

    feat(lista): criada função de criar nó

--- include/lista_url.h
+++ include/lista_url.h
@@ -11,6 +11,7 @@ struct no {
 
 typedef struct no t_lista;
 
+t_lista* criar_no(t_url* url);
 t_lista* inserir_url(t_lista* lista, t_url* url);
 void imprimir_lista(t_lista* lista);
 void liberar_lista(t_lista* lista);
--- src/lista_url.c
+++ src/lista_url.c
@@ -2,6 +2,14 @@
 
 #include <stdlib.h>
 
+t_lista* criar_no(t_url* url) {
+    t_lista* no = malloc(sizeof(t_lista));
+    no->url = url;
+    no->proximo = NULL;
+    no->anterior = NULL;
+    return no;
+}
+
 t_lista* inserir_url(t_lista* lista, t_url* url) {
     t_lista *no = malloc(sizeof(t_lista));
 
