e1bd62a1ddca909d21a4fe0e425c3ad3c8b226af
Anthonny Gabriell Marins Alves <anthonny14gabriell@gmail.com>
2026-05-08 15:15:10 -0400

    feat(navegacao): criado arquivo de cabeçalho para parte de navegação

---
+++ include/navegacao.h
@@ -0,0 +1,10 @@
+#ifndef GERENCIADOR_DE_URLS_NAVEGACAO_H
+#define GERENCIADOR_DE_URLS_NAVEGACAO_H
+
+#include "lista_url.h"
+
+void navegar(t_lista* lista);
+void avancar_pagina(t_lista* lista);
+void voltar_pagina(t_lista* lista);
+
+#endif //GERENCIADOR_DE_URLS_NAVEGACAO_H
