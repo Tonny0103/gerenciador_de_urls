6f93b5c7294896dc6c184d58d4c2fd9a69651d59
Anthonny Gabriell Marins Alves <anthonny14gabriell@gmail.com>
2026-05-09 15:34:07 -0400

    docs(navegacao): adicionado cabeçalho ao arquivo de cabeçalho

--- include/navegacao.h
+++ include/navegacao.h
@@ -1,3 +1,15 @@
+/*
+* =========================================
+ * Disciplina  : Estrutura de Dados
+ * Aluno       : Anthonny Gabriell Marins Alves
+ * Turma       : Engenharia da Computação 2026.1
+ * Data        : 09/05/2026
+ * =========================================
+ * Arquivo     : navegacao.h
+ * Descrição   : Arquivo de cabeçalho das funções referentes a navegação.
+ * =========================================
+ */
+
 #ifndef GERENCIADOR_DE_URLS_NAVEGACAO_H
 #define GERENCIADOR_DE_URLS_NAVEGACAO_H
 
