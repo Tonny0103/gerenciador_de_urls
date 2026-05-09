5ebbe99b360cea18ee519f56f0457d7cbdb88b9d
Anthonny Gabriell Marins Alves <anthonny14gabriell@gmail.com>
2026-05-09 16:08:48 -0400

    docs(main): arquivo principal do arquivo

--- main.c
+++ main.c
@@ -1,4 +1,15 @@
-#include "include/dados.h"
+/*
+* =========================================
+ * Disciplina  : Estrutura de Dados
+ * Aluno       : Anthonny Gabriell Marins Alves
+ * Turma       : Engenharia da Computação 2026.1
+ * Data        : 09/05/2026
+ * =========================================
+ * Arquivo     : main.c
+ * Descrição   : Arquivo principal do programa.
+ * =========================================
+ */
+
 #include "include/menu.h"
 
 int main(void) {
