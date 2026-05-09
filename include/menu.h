2f28da1871f58755d29a016818f287bc2ffccd56
Anthonny Gabriell Marins Alves <anthonny14gabriell@gmail.com>
2026-05-09 15:35:07 -0400

    docs(navegacao): adicionado cabeçalho nos arquivos de navegação

--- include/menu.h
+++ include/menu.h
@@ -1,3 +1,15 @@
+/*
+* =========================================
+ * Disciplina  : Estrutura de Dados
+ * Aluno       : Anthonny Gabriell Marins Alves
+ * Turma       : Engenharia da Computação 2026.1
+ * Data        : 09/05/2026
+ * =========================================
+ * Arquivo     : menu.h
+ * Descrição   : Arquivo de cabeçalho das funções referentes ao menu.
+ * =========================================
+ */
+
 #ifndef GERENCIADOR_DE_URLS_MENU_H
 #define GERENCIADOR_DE_URLS_MENU_H
 
--- src/menu.c
+++ src/menu.c
@@ -1,3 +1,15 @@
+/*
+* =========================================
+ * Disciplina  : Estrutura de Dados
+ * Aluno       : Anthonny Gabriell Marins Alves
+ * Turma       : Engenharia da Computação 2026.1
+ * Data        : 09/05/2026
+ * =========================================
+ * Arquivo     : menu.c
+ * Descrição   : Arquivo de implementação das funções referentes ao menu.
+ * =========================================
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
