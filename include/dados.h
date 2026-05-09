679c3cf5269aa43fa8db989d9274709de96ff261
Anthonny Gabriell Marins Alves <anthonny14gabriell@gmail.com>
2026-05-09 15:58:45 -0400

    docs(dados): adicionado cabeçalho nos arquivos de dados

--- include/dados.h
+++ include/dados.h
@@ -1,3 +1,15 @@
+/*
+* =========================================
+ * Disciplina  : Estrutura de Dados
+ * Aluno       : Anthonny Gabriell Marins Alves
+ * Turma       : Engenharia da Computação 2026.1
+ * Data        : 09/05/2026
+ * =========================================
+ * Arquivo     : dados.h
+ * Descrição   : Arquivo de cabeçalho das funções referentes ao acesso dos dados de persistência.
+ * =========================================
+ */
+
 #ifndef GERENCIADOR_DE_URLS_DADOS_H
 #define GERENCIADOR_DE_URLS_DADOS_H
 
--- src/dados.c
+++ src/dados.c
@@ -1,3 +1,15 @@
+/*
+* =========================================
+ * Disciplina  : Estrutura de Dados
+ * Aluno       : Anthonny Gabriell Marins Alves
+ * Turma       : Engenharia da Computação 2026.1
+ * Data        : 09/05/2026
+ * =========================================
+ * Arquivo     : dados.c
+ * Descrição   : Arquivo de implementação das funções referentes ao acesso dos dados de persistência.
+ * =========================================
+ */
+
 #include <stdio.h>
 #include "../include/dados.h"
 #include <string.h>
