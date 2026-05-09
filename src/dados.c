00d5303de599347ecedd6480280dbc02e7f1b367
Anthonny Gabriell Marins Alves <anthonny14gabriell@gmail.com>
2026-05-09 11:27:32 -0400

    feat(dados): ajustando buffer de entrada

--- src/dados.c
+++ src/dados.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 #include "../include/dados.h"
+#include <string.h>
 
 t_lista* obter_dados() {
     t_lista* lista = NULL;
     FILE* arquivo = fopen("dados.txt", "r");
 
     if (arquivo != NULL) {
-        char linha[256];
-        while (fscanf(arquivo, "%255s", linha) != EOF) {
+        char linha[2200];
+        while (fgets(linha, sizeof(linha), arquivo) != NULL) {
+            linha[strcspn(linha, "\n")] = '\0'; // remove o \n do final
             t_url* url = ler_url(linha);
             lista = inserir_url(lista, url);
         }
